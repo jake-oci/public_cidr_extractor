46c8d470b837362b5449fcd5d2be432b0cee85c7
Jake Bloom <126630123+jake-oci@users.noreply.github.com>
2023-03-07 15:37:14 -0500

    Add List Count for Each of the Print Blocks

--- cidr_extractor.py
+++ cidr_extractor.py
@@ -1,11 +1,5 @@
 import json, ipaddress, requests, argparse
 
-#Usage
-#python3 cidr_extractor.py --list OCI_DNS_NAME_1 OCI_DNS_NAME_2 OCI_DNS_NAME_3
-
-#Example for JMS
-#python3 cidr_extractor.py --list auth.us-ashburn-1.oraclecloud.com telemetry-ingestion.us-ashburn-1.oraclecloud.com management-agent.us-ashburn-1.oci.oraclecloud.com javamanagement-ingest.us-ashburn-1.oci.oraclecloud.com javamanagement.us-ashburn-1.oci.oraclecloud.com ingestion.logging.us-ashburn-1.oci.oraclecloud.com objectstorage.us-ashburn-1.oraclecloud.com
-
 #Requires dnspython to be installed
 try: 
     import dns.resolver
@@ -41,9 +35,6 @@ unique_ip_list=[]
 for x in list:
     if x not in unique_ip_list:
         unique_ip_list.append(x)
-print("")
-print("Unique IP's From DNS A Records")
-print(sorted(unique_ip_list))
 
 # Download Latest OCI JSON file
 url = 'https://docs.oracle.com/iaas/tools/public_ip_ranges.json'
@@ -59,7 +50,6 @@ if response.status_code == 200:
 else:
     print(f'Error retrieving JSON file: {response.status_code}')
 
-# Parse through the JSON and find matching CIDR blocks. Searches through all of the regions, just in case.
 cidr_blocks=[]
 regions_in_json=[]
 for ip_addr in unique_ip_list:
@@ -73,23 +63,22 @@ for ip_addr in unique_ip_list:
                 cidr_found=("{}").format(c['cidr'])
                 cidr_blocks.append(cidr_found)
 
-#Find the unique CIDR Blocks
 unique_cidr_blocks=[]
 for x in cidr_blocks:
     if x not in unique_cidr_blocks:
         unique_cidr_blocks.append(x)
-
-#Find the unique Regions
+    
 unique_regions=[]
 for x in regions_in_json:
     if x not in unique_regions:
         unique_regions.append(x)
 
 print("")
-print("CIDRs are from the following OCI Regions")
+print("Unique IP's From DNS A Records -- {}".format(len(unique_ip_list)))
+print(sorted(unique_ip_list))
+print("")
+print("CIDRs are from the following OCI Regions -- {}".format(len(unique_regions)))
 print(unique_regions)
 print("")
-print("Unique CIDR BLOCKS matched with OCI JSON file")
+print("Unique CIDR BLOCKS matched with OCI JSON file -- {}".format(len(unique_cidr_blocks)))
 print(sorted(unique_cidr_blocks))
-print("")
-print("There are {} unique CIDRs in this list.".format(len(unique_cidr_blocks)))
